02ffc608bda02b1798c4326c32d5a33e94dfa5d4
wangjx <wangjiaxiang97@foxmail.com>
2021-11-27 21:47:51 +0800

    第五章：继承 反射 添加 CopyOfTest、MethodTableTest、ObjectAnalyzer、ObjectAnalyzerTest

---
+++ src/v1ch05/objectAnalyzer/ObjectAnalyzerTest.java
@@ -0,0 +1,19 @@
+package v1ch05.objectAnalyzer;
+
+import java.util.*;
+
+/**
+ * This program uses reflection to spy on objects.
+ *
+ * @author Cay Horstmann
+ * @version 1.13 2018-03-16
+ */
+public class ObjectAnalyzerTest {
+    public static void main(String[] args)
+            throws ReflectiveOperationException {
+        ArrayList<Integer> squares = new ArrayList<Integer>();
+        for (int i = 1; i <= 5; i++)
+            squares.add(i * i);
+        System.out.println(new ObjectAnalyzer().toString(squares));
+    }
+}
