b5f67e564d66e22ab996bdc058ce372fefcfb629
wangjx <wangjiaxiang97@foxmail.com>
2021-12-16 17:23:59 +0800

    clone

--- src/MITest.java
+++ src/MITest.java
@@ -18,4 +18,73 @@ public class MITest implements MyImplement {
         String s1 = this.gettString() + s + "add string test";
         return s1;
     }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        TestClonePerson tcp = new TestClonePerson("wang",24,"2333332");
+        TestClonePerson tcp2 = (TestClonePerson) tcp.clone();
+        System.out.println(tcp.toString());
+        System.out.println(tcp2.toString());
+        tcp2.ds.setDescribe("jia");
+        tcp.ds.setDescribe("wangjiaxiang");
+        System.out.println(tcp.toString());
+        System.out.println(tcp2.toString());
+    }
+}
+
+class TestClonePerson implements Cloneable{
+    private String name;
+    private int age;
+    DescribePerson ds;
+
+    TestClonePerson(String name, int age, String ds) {
+        setAge(age);
+        setName(name);
+        this.ds = new DescribePerson(ds);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        TestClonePerson cloned = (TestClonePerson) super.clone();
+        cloned.ds = new DescribePerson(this.ds.getDescribe());
+        return cloned;
+    }
+
+    @Override
+    public String toString() {
+        return "TestClonePerson{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", ds=" + ds.getDescribe() +
+                '}';
+    }
+}
+
+class DescribePerson implements Cloneable{
+    private String describe;
+    DescribePerson(String ds) {
+        this.describe = ds;
+    }
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
 }
--- src/Nothing.java
+++ src/Nothing.java
@@ -1,11 +1,52 @@
-public class Nothing {
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Nothing implements Person, Named{
     public static void main(String[] args) {
 
         MITest test = new MITest("wangjiaxiang");
 
         System.out.println(test.testString("2333"));
+        BigDecimal x = new BigDecimal("1.0");
+        BigDecimal y = new BigDecimal("1.00");
+        System.out.println(x.equals(y));
+        System.out.println(x.compareTo(y));
+        Nothing nt = new Nothing();
+        System.out.println(nt.getName());
+        String[] a = {"sfsf", "fsdfsdf", "afdq", "sdqwd"};
+        Arrays.sort(a, new LengthComparator());
+        System.out.println(Arrays.toString(a));
+        Object o = new Object();
 
+        String aa = "wangjiaxiang";
+        System.out.println(aa.hashCode());
+        aa = "wangjiaxiangd";
+        System.out.println(aa.hashCode());
     }
 
+    @Override
+    public String getName() {
+        return Named.super.getName();
+    }
 }
 
+interface Person {
+    default String getName() {
+        return "";
+    }
+}
+
+interface Named {
+    default String getName() {
+        return getClass().getName() + "_" + hashCode();
+    }
+}
+
+class LengthComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length() - o2.length();
+    }
+}
